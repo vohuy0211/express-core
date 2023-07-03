79a6dce8417651f13442bd914ca1d7e893a59113
vohuy0211 <huyvo02112003@gmail.com>
2023-07-03 08:53:58 +0700

    baitap

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app/app');
 
 const port = 8080;
 app.listen(port, () => {
-  console.log(`listening on port htttp://localhost:${port}`);
+  console.log(`listening on port http://localhost:${port}`);
 });
